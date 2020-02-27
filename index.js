61ac7e7637fd59c04f344cf9684c7c667edf5910
Tracy Collins <tc@threeceemedia.com>
2020-02-27 08:18:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function breakOut(array, changeValue, stopValue) {
-
+  array.forEach((element,index, array) => {
+    if (element === stopValue){
+      break;
+    }
+      array[index] = changeValue
+  })
+  return array
 }
 
 function keepGoing(array, changeValue, skipValue) {
