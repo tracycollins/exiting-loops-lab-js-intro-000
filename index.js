9b2072640a85fac3f47101e7d0d987ab8089ff36
Tracy Collins <tc@threeceemedia.com>
2020-02-27 08:29:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,5 +19,10 @@ function keepGoing(array, changeValue, skipValue) {
 }
 
 function findBy(array, findFn) {
-
+  for(var i=0; i<array.length; i++){
+    if (findFn(array[i])){
+      return array[i]
+    }
+  }
+  return array
 }
