3a9c1df6142b1868cb062e335ffbb8fd1fa230c4
Tracy Collins <tc@threeceemedia.com>
2020-02-27 08:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function breakOut(array, changeValue, stopValue) {
     if (array[i] === stopValue){
       break
     }
-    array[index] = changeValue
+    array[i] = changeValue
   }
   return array
 }
